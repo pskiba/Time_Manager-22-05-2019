701c5914160bac8755438573220135582c7938dd
pskiba <piotr.skiba@oracle.com>
2019-08-27 13:05:40 +0200

    with build

---
+++ backend/build/precache-manifest.a6eaf1170da2789a2347e1bb1f820ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2751fc7087948cc5822c1dae8ba23b74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d066f7e11c32140936a5",
+    "url": "/static/js/2.b1c6d2cd.chunk.js"
+  },
+  {
+    "revision": "a3d2e262452b3986c894",
+    "url": "/static/js/main.3c4b476f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77280cfd22cd7b935c8e3ff7cc120910",
+    "url": "/static/media/checkmarkIcon.77280cfd.svg"
+  },
+  {
+    "revision": "276a131657dea7fcede0f38bafeb38d3",
+    "url": "/static/media/editIcon.276a1316.svg"
+  },
+  {
+    "revision": "4b8a3343b4dcf492f564a2270cdcc29f",
+    "url": "/static/media/trashIcon.4b8a3343.svg"
+  }
+]);
